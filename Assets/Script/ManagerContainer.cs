5064af6097c659f5560228b779fb928108ace530
Tony <tonynz94@naver.com>
2022-04-19 01:21:12 +0900

    Add ManagerContainer

---
+++ Assets/Script/ManagerContainer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerContainer : MonoBehaviour
+{
+    public static ManagerContainer s_Instance;
+    public static ManagerContainer Instance { get { Init(); return s_Instance; } }
+
+
+    public static void Init()
+    { 
+        if(s_Instance == null)
+        {
+            GameObject go =GameObject.Find("@ManagerContainer");
+
+            if(go == null)
+            {
+                go = new GameObject { name = "@ManagerContainer" };
+
+
+                go.AddComponent<ManagerContainer>();
+            }
+            s_Instance = go.GetComponent<ManagerContainer>();
+            DontDestroyOnLoad(go);
+        }
+    }
+}
